a772cd1133f206b088d46741bfd7b583202f18d4
Ben Draut <drautb@gmail.com>
2015-12-03 20:40:56 -0700

    First impl of indexer done

---
+++ haystack/indexer.py
@@ -0,0 +1,138 @@
+import exifread
+import hashlib
+import logging
+import os
+import re
+import shutil
+
+from config import Config
+from date_taken_extractor import DateTakenExtractor
+from datetime import datetime
+from file import File
+from index import Index
+from PIL import Image
+from time import strptime
+
+BLOCK_SIZE_1M = 1048576  # 1024 Bytes * 1024 Bytes = 1M
+READ_ONLY_RAW = 'rb'
+
+PATTERN_REPLACE_YEAR = '%Y'
+PATTERN_REPLACE_MONTH = '%M'
+PATTERN_REPLACE_DAY = '%D'
+
+
+class Indexer:
+    def __init__(self, config=None, index=None, date_taken_extractor=None, thumbnail_generator=None):
+        if config is None:
+            config = Config()
+
+        if index is None:
+            index = Index()
+
+        if date_taken_extractor is None:
+            date_taken_extractor = DateTakenExtractor()
+
+        if thumbnail_generator is None:
+            thumbnail_generator = ThumbnailGenerator()
+
+        self.config = config
+        self.index = index
+        self.date_taken_extractor = date_taken_extractor
+        self.thumbnail_generator = thumbnail_generator
+
+    def run(self):
+        logging.info('Indexer started.')
+
+        # Locate the staging directory.
+        staging_root = self.config.staging_root()
+        logging.info('Located staging directory. staging_root=%s', staging_root)
+
+        for staging_dir in self.__device_staging_dirs(staging_root):
+            self.__index_files(staging_dir)
+
+    def __device_staging_dirs(self, staging_root):
+        device_staging_dirs = []
+
+        # For each device folder in the directory
+        for f in os.listdir(staging_root):
+            path_to_device_dir = self.config.staging_directory(f)
+            if not os.path.isdir(path_to_device_dir):
+                logging.info('Found non-directory file in staging root, skipping. staging_root=%s file=%s',
+                             staging_root, path_to_device_dir)
+            else:
+                device_staging_dirs.append(f)
+
+        return device_staging_dirs
+
+    def __index_files(self, device_dir):
+        staging_dir = self.config.staging_directory(device_dir)
+        for filename in os.listdir(staging_dir):
+            if os.path.isdir(filename):
+                logging.error('found unexpected subdirectory in device staging directory, ignoring. ' +
+                              'dir=%s staging_dir=%s', f, staging_dir)
+                continue
+
+            path_to_file = os.path.join(staging_dir, filename)
+            if File(path_to_file).is_image():
+                self.__index_file(device_dir, path_to_file)
+            else:
+                logging.info('File is not an image, not indexing. file=%s staging_dir=%s', path_to_file, staging_dir)
+
+    def __index_file(self, device, path_to_file):
+        logging.info('Indexing file=%s', path_to_file)
+        file_hash = self.__get_media_hash(path_to_file)
+
+        if self.index.is_duplicate(file_hash):
+            logging.info('File hash already appears in index, file appears to be a duplicate, and will be deleted. ' +
+                         'path_to_file=%s file_hash=%s', path_to_file, file_hash)
+            os.remove(path_to_file)
+            return
+
+        try:
+            f = File(path_to_file)
+
+            # Get the date the media was taken.
+            date_taken = self.date_taken_extractor.get_date_taken(path_to_file)
+
+            # Generate Thumbnail.
+            path_to_thumbnail = self.__generate_path_to_thumbnail(f, date_taken, file_hash)
+            self.thumbnail_generator.generate_thumbnail(path_to_file, path_to_thumbnail)
+
+            # Copy file to final place.
+            path_to_final_file = self.__generate_path_to_final_file(f, date_taken, file_hash)
+            logging.info('Copying staged media to final location. path_to_staged_file=%s path_to_final_file=%s',
+                         path_to_file, path_to_final_file)
+            shutil.copy(path_to_file, path_to_final_file)
+
+            # Send data to index.
+            self.index.index_media(path_to_final_file, path_to_thumbnail, date_taken, device, file_hash, f.media_type())
+
+            # Remove file after successful indexing.
+            logging.info('Removing file=%s', path_to_file)
+            os.remove(path_to_file)
+        except Exception as e:
+            logging.error('Encountered error while trying to index file, leaving file in staging area. path_to_file=%s',
+                          path_to_file, e)
+
+    def __get_media_hash(self, path_to_file):
+        md5 = hashlib.md5()
+        with open(path_to_file, READ_ONLY_RAW) as f:
+            for chunk in iter(lambda: f.read(BLOCK_SIZE_1M), b''):
+                md5.update(chunk)
+
+        return md5.hexdigest()
+
+    def __generate_path_to_thumbnail(self, f, date_taken, hash):
+        path_to_thumbnail_pattern = self.config.thumbnail_path_pattern()
+        return self.__generate_path_to_file(path_to_thumbnail_pattern, f, date_taken, hash)
+
+    def __generate_path_to_final_file(self, f, date_taken, hash):
+        path_to_final_file_pattern = self.config.picture_path_pattern()
+        return self.__generate_path_to_file(path_to_final_file_pattern, f, date_taken, hash)
+
+    def __generate_path_to_file(self, pattern, f, date_in_seconds, file_hash):
+        date = datetime.fromtimestamp(date_in_seconds)
+        pattern = pattern.replace(PATTERN_REPLACE_YEAR, str(date.year))
+        pattern = pattern.replace(PATTERN_REPLACE_MONTH, str(date.month))
+        pattern = pattern.replace(PATTERN_REPLACE_DAY, str(date.day))
+        return os.path.join(pattern, file_hash + f.ext())
---
+++ haystack/test/test_indexer.py
@@ -0,0 +1,139 @@
+import unittest
+
+from config import Config
+from date_taken_extractor import DateTakenExtractor
+from file import File
+from index import Index
+from indexer import Indexer
+from mock import ANY
+from mock import MagicMock
+from mock import Mock
+from mock import mock_open
+from mock import patch
+from thumbnail_generator import ThumbnailGenerator
+
+
+def mock_listdir(*args):
+    mapping = {('/staging',): ['device-serial-1'],
+               ('/staging/device-serial-1',): ['file.jpg', 'movie.mp4']}
+    if args in mapping:
+        return mapping[args]
+    else:
+        return False
+
+
+def mock_isdir(*args):
+    mapping = {('/staging/device-serial-1',): True}
+    if args in mapping:
+        return mapping[args]
+    else:
+        return False
+
+
+def mock_staging_dir(*args):
+    return '/staging/{0}'.format(args[0])
+
+
+class TestIndexer(unittest.TestCase):
+    def setUp(self):
+        self.mock_listdir_patcher = patch('os.listdir')
+        self.mock_listdir = self.mock_listdir_patcher.start()
+        self.mock_listdir.side_effect = mock_listdir
+
+        self.mock_isdir_patcher = patch('os.path.isdir')
+        self.mock_isdir = self.mock_isdir_patcher.start()
+        self.mock_isdir.side_effect = mock_isdir
+
+        self.mock_copy_patcher = patch('shutil.copy')
+        self.mock_copy = self.mock_copy_patcher.start()
+
+        self.mock_open_patcher = patch('indexer.open')
+        self.mock_open = self.mock_open_patcher.start()
+
+        # http://stackoverflow.com/questions/24779893/customizing-unittest-mock-mock-open-for-iteration
+        self.mock_open.return_value = mock_open(read_data='fake-file-contents').return_value
+
+        self.mock_remove_patcher = patch('os.remove')
+        self.mock_remove = self.mock_remove_patcher.start()
+
+        self.mock_config = Mock(spec=Config)
+        self.mock_config.staging_root.return_value = '/staging'
+        self.mock_config.thumbnail_path_pattern.return_value = '/thumbnails/%Y/%M/%D'
+        self.mock_config.picture_path_pattern.return_value = '/pictures/%Y/%M/%D'
+        self.mock_config.staging_directory.side_effect = mock_staging_dir
+
+        self.mock_date_taken_extractor = Mock(spec=DateTakenExtractor)
+        self.mock_date_taken_extractor.get_date_taken.return_value = 1449176000
+
+        self.mock_index = Mock(spec=Index)
+        self.mock_index.is_duplicate.return_value = False
+
+        self.mock_thumbnail_generator = Mock(spec=ThumbnailGenerator)
+
+        self.test_model = Indexer(self.mock_config, self.mock_index, self.mock_date_taken_extractor,
+                                  self.mock_thumbnail_generator)
+
+    def tearDown(self):
+        self.mock_listdir_patcher.stop()
+        self.mock_isdir_patcher.stop()
+        self.mock_copy_patcher.stop()
+        self.mock_open_patcher.stop()
+        self.mock_remove_patcher.stop()
+
+    def test_it_should_generate_a_thumbnail_using_the_expected_path_to_thumbnail(self):
+        expected_path_to_thumbnail = '/thumbnails/2015/12/3/6c8abb37a65a74b526d456927a19549d.jpg'
+        self.test_model.run()
+        self.mock_thumbnail_generator.generate_thumbnail.assert_called_once_with(ANY, expected_path_to_thumbnail)
+
+    def test_it_should_generate_a_thumbnail_using_the_expected_path_to_file(self):
+        expected_path_to_file = '/staging/device-serial-1/file.jpg'
+        self.test_model.run()
+        self.mock_thumbnail_generator.generate_thumbnail.assert_called_once_with(expected_path_to_file, ANY)
+
+    def test_it_should_copy_media_from_the_staging_location_to_the_final_location(self):
+        self.test_model.run()
+        self.mock_copy.assert_called_once_with('/staging/device-serial-1/file.jpg',
+                                               '/pictures/2015/12/3/6c8abb37a65a74b526d456927a19549d.jpg')
+
+    def test_it_should_index_media_with_the_right_final_path(self):
+        expected_path_to_file = '/pictures/2015/12/3/6c8abb37a65a74b526d456927a19549d.jpg'
+        self.test_model.run()
+        self.mock_index.index_media.assert_called_once_with(expected_path_to_file, ANY, ANY, ANY, ANY, ANY)
+
+    def test_it_should_index_media_with_the_right_thumbnail_path(self):
+        expected_path_to_thumbnail = '/thumbnails/2015/12/3/6c8abb37a65a74b526d456927a19549d.jpg'
+        self.test_model.run()
+        self.mock_index.index_media.assert_called_once_with(ANY, expected_path_to_thumbnail, ANY, ANY, ANY, ANY)
+
+    def test_it_should_index_media_with_the_right_date_taken(self):
+        self.test_model.run()
+        self.mock_index.index_media.assert_called_once_with(ANY, ANY, 1449176000, ANY, ANY, ANY)
+
+    def test_it_should_index_media_with_the_right_device_id(self):
+        self.test_model.run()
+        self.mock_index.index_media.assert_called_once_with(ANY, ANY, ANY, 'device-serial-1', ANY, ANY)
+
+    def test_it_should_index_media_with_the_right_hash(self):
+        self.test_model.run()
+        self.mock_index.index_media.assert_called_once_with(ANY, ANY, ANY, ANY, '6c8abb37a65a74b526d456927a19549d', ANY)
+
+    def test_it_should_index_media_with_the_right_media_type(self):
+        self.test_model.run()
+        self.mock_index.index_media.assert_called_once_with(ANY, ANY, ANY, ANY, ANY, 'JPG')
+
+    def test_it_should_delete_staged_media_after_indexing(self):
+        self.test_model.run()
+        self.mock_remove.assert_called_once_with('/staging/device-serial-1/file.jpg')
+
+    def test_it_should_not_delete_staged_media_if_an_error_occurred_during_indexing(self):
+        self.mock_index.index_media.side_effect = RuntimeError
+        self.test_model.run()
+        self.mock_remove.assert_not_called()
+
+    def test_it_should_not_index_videos_yet(self):
+        self.test_model.run()
+        self.assertEqual(self.mock_open.call_count, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
