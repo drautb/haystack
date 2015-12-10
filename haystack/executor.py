02fb030a466a8ac4fa44dcf1da15a51314305673
Ben Draut <drautb@gmail.com>
2015-12-09 19:01:30 -0700

    Add log for executor

--- haystack/executor.py
+++ haystack/executor.py
@@ -1,6 +1,8 @@
+import logging
 import subprocess
 
 
 class Executor:
     def execute(self, args):
+        logging.info('Executing command. args=%s', str(args))
         return subprocess.check_call(args)
