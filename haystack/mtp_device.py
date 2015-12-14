15693207c9f7ecd7cd77b90eafd45295b1dced96
Ben Draut <drautb@gmail.com>
2015-12-14 09:12:36 -0700

    Consume new MTP Driver

--- haystack/mtp_device.py
+++ haystack/mtp_device.py
@@ -24,24 +24,20 @@ class MTPDevice:
         self.util = util
 
     def transfer_media(self):
-        if not self.mtp.detect_devices():
+        device_info = self.mtp.detect_device()
+        if not device_info:
             logging.info('No MTP devices connected, skipping MTP media transfer.')
             return
 
-        # Connect to the first connected device.
-        logging.info('Connecting to MTP device.')
-        try:
-            self.mtp.connect()
-            self.__log_connected_msg()
-        except:
-            logging.exception('Error connecting to MTP device! Skipping MTP media transfer.')
-            return
+        self.__log_connected_msg(device_info)
+
+        folders = self.mtp.get_folder_list()
 
         # Get the IDs for the parent folders to index.
-        parent_folder_ids = self.__find_parent_folder_ids()
+        parent_folder_ids = self.__find_parent_folder_ids(folders)
 
         # Find all folder ids in the parent folders
-        folder_ids = self.__find_all_folder_ids(parent_folder_ids)
+        folder_ids = self.__find_all_folder_ids(parent_folder_ids, folders)
 
         # Find all media files in these folders
         media_files = self.__find_files_in_folders(folder_ids)
@@ -49,48 +45,38 @@ class MTPDevice:
             logging.info('No files found to transfer.')
 
         # Transfer all files from the folders
-        dest_dir = self.__get_staging_dir()
+        dest_dir = self.__get_staging_dir(device_info['serial'])
         self.util.mkdirp(dest_dir)
         for f in media_files:
             self.__transfer_file(f, dest_dir)
 
-        # Disconnect
-        logging.info('Disconnecting from MTP device.')
-        self.mtp.disconnect()
-
-    def __log_connected_msg(self):
-        msg_str = "Connected to device. devicename={} serialnumber={} manufacturer={} modelname={} deviceversion={}"
-        msg = msg_str.format(self.mtp.get_devicename(),
-                             self.mtp.get_serialnumber(),
-                             self.mtp.get_manufacturer(),
-                             self.mtp.get_modelname(),
-                             self.mtp.get_deviceversion())
+    def __log_connected_msg(self, info):
+        msg_str = "Connected to device. serialnumber={} manufacturer={} modelname={}"
+        msg = msg_str.format(info['serial'], info['manufacturer'], info['model'])
         logging.info(msg)
 
-    def __find_parent_folder_ids(self):
+    def __find_parent_folder_ids(self, folders):
         ids = []
         folders_to_index = self.config.mtp_media_directories()
         logging.info('Folders to index: %s', folders_to_index)
-        for f in self.mtp.get_parent_folders():
-            if f.name in folders_to_index:
-                ids.append(f.folder_id)
+        for f in folders:
+            if f['name'] in folders_to_index:
+                ids.append(f['id'])
 
         return ids
 
-    def __find_all_folder_ids(self, parent_folder_ids):
+    def __find_all_folder_ids(self, parent_folder_ids, complete_folder_list):
         logging.info('Finding all ids for folders in parent folders: %s', parent_folder_ids)
         folder_ids = set(parent_folder_ids)
-        complete_folder_list = self.mtp.get_folder_list()
 
         current_length = len(folder_ids)
         new_length = None
         while current_length != new_length:
             current_length = len(folder_ids)
 
-            for key in complete_folder_list:
-                f = complete_folder_list[key]
-                if f.parent_id in folder_ids:
-                    folder_ids.add(f.folder_id)
+            for f in complete_folder_list:
+                if f['parent_id'] in folder_ids:
+                    folder_ids.add(f['id'])
 
             new_length = len(folder_ids)
 
@@ -101,34 +87,34 @@ class MTPDevice:
         files_in_folders = []
 
         for f in self.mtp.get_filelisting():
-            if f.parent_id in folder_ids:
+            if f['parent_id'] in folder_ids:
                 files_in_folders.append(f)
 
         return files_in_folders
 
-    def __get_staging_dir(self):
-        return self.config.staging_directory(self.mtp.get_serialnumber())
+    def __get_staging_dir(self, serial):
+        return self.config.staging_directory(serial)
 
     def __transfer_file(self, src_file, dest_dir):
         try:
-            f = File(src_file.filename)
+            f = File(src_file['name'])
         except RuntimeError:
-            logging.warn('Found unrecognized file in folders to index, skipping. filename=%s', src_file.filename)
+            logging.warn('Found unrecognized file in folders to index, skipping. filename=%s', src_file['name'])
             return
 
-        dest_file = '{}/{}'.format(dest_dir, src_file.filename)
+        dest_file = '{}/{}'.format(dest_dir, src_file['name'])
         logging.info('Downloading file from device. file_id=%s filename=%s destination=%s',
-                     src_file.item_id, src_file.filename, dest_file)
-        self.mtp.get_file_to_file(src_file.item_id, dest_file)
+                     src_file['id'], src_file['name'], dest_file)
+        self.mtp.get_file_to_file(src_file['id'], dest_file)
 
         # Make sure it's successful before deleting.
         if not os.path.isfile(dest_file):
             logging.info('Download failed! dest_file was does not exist. file_id=%s dest_file=%s',
-                         src_file.item_id, dest_file)
-        elif os.path.getsize(dest_file) != src_file.filesize:
+                         src_file['id'], dest_file)
+        elif os.path.getsize(dest_file) != src_file['size']:
             logging.info('Download failed! dest_file size does not match expected.' +
                          ' file_id=%s dest_file=%s dest_file_size=%s src_file_size=%s',
-                         src_file.item_id, dest_file, os.path.getsize(dest_file), src_file.filesize)
+                         src_file['id'], dest_file, os.path.getsize(dest_file), src_file['size'])
         else:
             logging.info('Download succeeded, deleting file from dest_file=%s', dest_file)
-            self.mtp.delete_object(src_file.item_id)
+            self.mtp.delete_object(src_file['id'])
--- haystack/test/test_mtp_device.py
+++ haystack/test/test_mtp_device.py
@@ -2,45 +2,39 @@ import unittest
 import pymtp
 import util
 
+from config import Config
 from mock import ANY
 from mock import MagicMock
 from mock import patch
 from mtp_device import MTPDevice
-from pymtp import LIBMTP_Folder
-from pymtp import LIBMTP_File
+from mtp_driver import MTPDriver
 
 
 class TestMTPDevice(unittest.TestCase):
 
-    def mock_folder(self, name, id, parent_id=0):
-        f = MagicMock(spec=LIBMTP_Folder)
-        f.name = name
-        f.folder_id = id
-        f.parent_id = parent_id
-        return f
+    def mock_folder(self, name, id, parent_id=None):
+        return {
+            'id': id,
+            'name': name,
+            'parent_id': parent_id
+        }
 
     def mock_file(self, name, id, parent_id, size):
-        f = MagicMock(spec=LIBMTP_File)
-        f.filename = name
-        f.item_id = id
-        f.parent_id = parent_id
-        f.filesize = size
-        return f
-
-    def setUp_mtp_get_parent_folders(self, mock_mtp):
-        parent_folders = [self.mock_folder('Downloads', 1),
-                          self.mock_folder('DCIM', 5),
-                          self.mock_folder('Blah', 10)]
-        mock_mtp.get_parent_folders.return_value = parent_folders
+        return {
+            'id': id,
+            'name': name,
+            'parent_id': parent_id,
+            'size': size
+        }
 
     def setUp_mtp_get_folder_list(self, mock_mtp):
-        folder_list = {1: self.mock_folder('Downloads', 1),
-                       2: self.mock_folder('DCIM', 5),
-                       3: self.mock_folder('Blah', 10),
-                       4: self.mock_folder('Camera', 15, 5),
-                       5: self.mock_folder('alternate', 20, 15),
-                       6: self.mock_folder('thumbnails', 25, 5),
-                       7: self.mock_folder('Stuff', 30, 10)}
+        folder_list = [self.mock_folder('Downloads', 1),
+                       self.mock_folder('DCIM', 5),
+                       self.mock_folder('Blah', 10),
+                       self.mock_folder('Camera', 15, 5),
+                       self.mock_folder('alternate', 20, 15),
+                       self.mock_folder('thumbnails', 25, 5),
+                       self.mock_folder('Stuff', 30, 10)]
         mock_mtp.get_folder_list.return_value = folder_list
 
     def setUp_mtp_get_filelisting(self, mock_mtp):
@@ -49,38 +43,28 @@ class TestMTPDevice(unittest.TestCase):
         mock_mtp.get_filelisting.return_value = file_listing
 
     def setUp(self):
-        self.mock_config = MagicMock()
+        self.mock_config = MagicMock(spec=Config)
         self.mock_config.mtp_media_directories.return_value = ['DCIM', 'Pictures']
         self.mock_config.haystack_root.return_value = '/haystack'
         self.mock_config.staging_directory.return_value = '/haystack/staging/serial-number'
 
-        self.mock_mtp = MagicMock(spec=pymtp.MTP)
-        self.setUp_mtp_get_parent_folders(self.mock_mtp)
+        self.mock_mtp = MagicMock(spec=MTPDriver)
         self.setUp_mtp_get_folder_list(self.mock_mtp)
         self.setUp_mtp_get_filelisting(self.mock_mtp)
-        self.mock_mtp.get_devicename.return_value = 'my-device'
-        self.mock_mtp.get_serialnumber.return_value = 'serial-number'
-        self.mock_mtp.get_manufacturer.return_value = 'LG'
-        self.mock_mtp.get_modelname.return_value = 'Moto X'
-        self.mock_mtp.get_deviceversion.return_value = '1.0'
+        self.mock_mtp.detect_device.return_value = {
+            'manufacturer': 'LG',
+            'serial': 'serial-number',
+            'model': 'Moto X'
+        }
 
         self.mock_util = MagicMock(spec=util.Util)
 
         self.test_model = MTPDevice(self.mock_config, self.mock_mtp, self.mock_util)
 
-    def test_transfer_media_should_handle_connection_errors(self):
-        self.mock_mtp.connect.side_effect = Exception('Some connection error')
-        self.test_model.transfer_media()
-
-    def test_transfer_media_should_not_disconnect_if_connect_failed(self):
-        self.mock_mtp.connect.side_effect = Exception('Some connection error')
-        self.test_model.transfer_media()
-        self.mock_mtp.disconnect.assert_not_called()
-
     def test_transfer_media_should_do_nothing_if_no_devices_are_connected(self):
-        self.mock_mtp.detect_devices.return_value = []
+        self.mock_mtp.detect_device.return_value = None
         self.test_model.transfer_media()
-        self.mock_mtp.connect.assert_not_called()
+        self.mock_mtp.get_folder_list.assert_not_called()
 
     def test_transfer_media_should_only_transfer_media_from_proper_directories(self):
         self.test_model.transfer_media()
