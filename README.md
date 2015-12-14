468fe01c5778409a12b5a469c6a24c12dedc15a7
Ben Draut <drautb@gmail.com>
2015-12-14 16:03:59 -0700

    Some project cleanup

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ Haystack
 ## Usage
 
 ```sh
+# Start the indxer
 sudo ./bin/haystack {start|stop|restart|status}
+
+# Start the static file server
+sudo ./bin/haystack-server {start|stop|restart|status}
 ```
 
 ## Setup
 
 * `install.sh` will prepare the Raspberry Pi to run haystack.
+* Make sure the values in `config.properties` and `lighttpd.conf` are correct.
 
 ## Development Notes
 
---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+# There may be things missing here, or things that are here but aren't needed anymore.
+
+sudo apt-get update
+
+sudo apt-get install daemon \
+                     exiftool \
+                     libavcodec-dev \
+                     libavformat-dev \
+                     libavutil-dev \
+                     libjpeg-dev \
+                     libmtp-dev \
+                     libswscale-dev \
+                     lighttpd \
+                     mtp-tools \
+                     python \
+                     python-dev \
+                     python-pip
+
+sudo pip install configparser \
+                 funcsigs \
+                 mock \
+                 pbr \
+                 pillow \
+                 python-dateutil \
+                 python-firebase \
+                 requests
+
+# ExifTool Wrapper
+git clone https://github.com/smarnach/pyexiftool.git /tmp/pyexiftool
+cd /tmp/pyexiftool && sudo python setup.py install
+
+# Compile/install ffmpeg from src
+# https://www.bitpi.co/2015/08/19/how-to-compile-ffmpeg-on-a-raspberry-pi/
